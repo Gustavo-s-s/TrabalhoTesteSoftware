0c0a5af3848fb5a8e0f3b295ae2f46d16859f604
Gustavo Santos Silva <80991684+Gustavo-s-s@users.noreply.github.com>
2022-04-26 18:57:57 -0300

    Add files via upload

---
+++ CentroDistribuicao.java
@@ -0,0 +1,97 @@
+public class CentroDistribuicao {
+    public enum SITUACAO { NORMAL, SOBRAVISO, EMERGENCIA }
+    public enum TIPOPOSTO { COMUM, ESTRATEGICO }
+
+    public static final int MAX_ADITIVO = 500;
+    public static final int MAX_ALCOOL = 2500;
+    public static final int MAX_GASOLINA = 10000;
+
+    public int ad;
+    public int gas;
+    public int al1;
+    public int al2;
+    public CentroDistribuicao.SITUACAO situacao;
+
+    public CentroDistribuicao (int tAditivo, int tGasolina, int tAlcool1, int tAlcool2) { 
+        ad = 0;
+        gas = 0;
+        al1 = 0;
+        al2 = 0;
+        
+        recebeAditivo(tAditivo);
+        recebeGasolina(tGasolina);
+        recebeAlcool((tAlcool1 + tAlcool2)/2);
+        defineSituacao();
+     }
+
+    public void defineSituacao(){
+        if(ad < MAX_ADITIVO * 0.25 || gas < MAX_GASOLINA * 0.25 || al1 < MAX_ALCOOL/2 * 0.25 || al2 < MAX_ALCOOL/2 * 0.25 ){
+            situacao = SITUACAO.EMERGENCIA;
+        } else if (ad < MAX_ADITIVO * 0.5 || gas < MAX_GASOLINA * 0.5 || al1 < MAX_ALCOOL/2 * 0.5 || al2 < MAX_ALCOOL/2 * 0.5 ){
+            situacao = SITUACAO.SOBRAVISO;
+        } else {
+            situacao = SITUACAO.NORMAL;
+        }
+     }
+
+    public SITUACAO getSituacao(){
+        return situacao;
+    }
+
+    public int gettGasolina(){
+        return gas;
+    }
+
+    public int gettAditivo(){
+        return ad;
+    }
+
+    public int gettAlcool1(){
+        return al1;
+    }
+
+    public int gettAlcool2(){
+        return al2;
+    }
+
+    public int recebeAditivo(int qtdade) {
+        if(qtdade < 0){
+            return -1;
+        }else if(qtdade + ad >= MAX_ADITIVO){
+            ad = MAX_ADITIVO;
+            return MAX_ADITIVO - ad;
+        }else{
+            ad = qtdade + ad;
+            return qtdade;
+        }
+    }
+
+    public int recebeGasolina(int qtdade) {
+        if(qtdade < 0){
+            return -1;
+        }else if(qtdade + gas >= MAX_GASOLINA){
+            gas = MAX_GASOLINA;
+            return MAX_GASOLINA - gas;
+        }else{
+            gas = qtdade + gas;
+            return qtdade;
+        }
+    }
+
+    public int recebeAlcool(int qtdade) {
+        if(qtdade < 0){
+            return -1;
+        }else if(qtdade + al1 >= MAX_ALCOOL/2){
+            al1 = MAX_ALCOOL/2;
+            al2 = MAX_ALCOOL/2;
+            return MAX_ALCOOL/2 - al1;
+        }else{
+            al1 = qtdade + al1;
+            al2 = qtdade + al2;
+            return qtdade;
+        }
+    }
+
+    public int[] encomendaCombustivel(int qtdade, TIPOPOSTO tipoPosto) {
+        
+}
